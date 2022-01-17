bb94c4255ef0a7cfe36d8b260455531d4f32693b
Ryula <hobby519@naver.com>
2022-01-17 21:14:57 +0900

    data 추가

---
+++ reactjs/src/commons/components/data/data.js
@@ -0,0 +1,30 @@
+const list=[
+    {
+        id: 1,
+        title: "13 솝 언럭키 포 더트",
+        price: 16000,
+        img: 'https://www.lush.co.kr/data/goods/20/07/31/1000001306/1000001306_main_086.jpg',
+        desc: "#솝 #피부를지켜주는오레가노"
+    },
+    {
+        id: 2,
+        title: "오렌지 330g/665g",
+        price: 30000,
+        img: 'https://www.lush.co.kr/data/goods/20/09/39/1000001344/1000001344_main_054.jpg',
+        desc: "#샤워스크럽 #갓짜낸오렌지주스"
+    },
+    {
+        id: 3,
+        title: "팅글 225g",
+        price: 44000,
+        img: 'https://www.lush.co.kr/data/goods/20/01/03/1000001142/1000001142_main_088.jpg',
+        desc: "#보디컨디셔너 #오감을깨우는페퍼민트"
+    },
+    {
+        id: 4,
+        title: "로즈 아르간 보디 컨디셔너 225g/450g",
+        price: 44000,
+        img: 'https://www.lush.co.kr/data/goods/17/10/23/333/333_main_031.jpg',
+        desc: "#보디컨디셔너 #장미꽃밭에누은듯"
+    }
+]
