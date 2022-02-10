e6a64538232659706d1223c0685a64c7b1b14a13
Jiyun Kong <kjyn2001@gmail.com>
2022-02-10 21:59:06 +0900

    new pull request

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -2,19 +2,21 @@ import './App.css';
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Main from './commons/components/main/Main';
 import List from './commons/components/list';
-import Menu from './commons/components/menu'
+import Menu from './commons/components/menu';
+import Footer from './commons/components/main/Footer';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Menu></Menu>
-          <Routes>
-            <Route path="/main" exact element={<Main/>}></Route>
-            <Route path="/list" exact element={<List/>}></Route>
-          </Routes>
-        </BrowserRouter>
-
+        <Routes>
+          <Route path="/" exact element={<Main />}></Route>
+          <Route path="/main" exact element={<Main />}></Route>
+          <Route path="/list" exact element={<List />}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
 
 
     </div>
